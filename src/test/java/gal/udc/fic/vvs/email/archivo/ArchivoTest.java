d8bd6d2fd21234368d2ea408e1fd71c44a143e21
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-10 17:23:20 +0100

    Estructura de Test de Unidad: Archivo terminada.

---
+++ src/test/java/gal/udc/fic/vvs/email/archivo/ArchivoTest.java
@@ -0,0 +1,14 @@
+package gal.udc.fic.vvs.email.archivo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArchivoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
