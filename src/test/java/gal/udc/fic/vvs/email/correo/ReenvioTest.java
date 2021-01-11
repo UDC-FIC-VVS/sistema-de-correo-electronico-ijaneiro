a2f664fb50a7e22a7ee6a2f7428d30bd87892428
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-11 23:16:12 +0100

    Pruebas de Unidad: Reenvio.

--- src/test/java/gal/udc/fic/vvs/email/correo/ReenvioTest.java
+++ src/test/java/gal/udc/fic/vvs/email/correo/ReenvioTest.java
@@ -1,14 +1,40 @@
 package gal.udc.fic.vvs.email.correo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class ReenvioTest {
+import gal.udc.fic.vvs.email.archivo.Texto;
 
+public class ReenvioTest {
+	
+	private Mensaje mensaje = new Mensaje(new Texto("Correo1", "Correo de prueba"));
+	private Mensaje mensajeReenviar = new Mensaje(new Texto("Correo2", "Correo a reenviar"));
+	
+	private DecoradorMensaje reenvio;
+
+	@Before
+	public void init() {
+		reenvio = new Reenvio(mensaje, mensajeReenviar);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void obtenerTamañoTest() {
+		
+		int tamaño = mensaje.obtenerTamaño() + mensajeReenviar.obtenerTamaño();
+
+		assertEquals(tamaño, reenvio.obtenerTamaño());
+	}
+	
+	@Test
+	public void obtenerVisualizacionTest() {
+		
+		String visualizacion = mensaje.obtenerVisualizacion() + "\n\n---- Correo reenviado ----\n\n" + 
+							mensajeReenviar.obtenerVisualizacion() + "\n---- Fin correo reenviado ----";
+		
+		assertEquals(visualizacion, reenvio.obtenerVisualizacion());
 	}
 
 }
