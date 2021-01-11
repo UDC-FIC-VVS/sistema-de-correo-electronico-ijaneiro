0c93d73b3ea9c4a11b6ab2de4fcb23a6402a6e95
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-11 16:35:56 +0100

    Pruebas de Unidad: Audio.

--- src/test/java/gal/udc/fic/vvs/email/archivo/AudioTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/AudioTest.java
@@ -1,14 +1,53 @@
 package gal.udc.fic.vvs.email.archivo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AudioTest {
+	
+	private static final String NOMBRE_ARCHIVO = "AudioTest";
+	private static final String CONTENIDO_ARCHIVO = "AudioTestContenido";
+	private static final int TAMAÑO_ARCHIVO = CONTENIDO_ARCHIVO.length();
+	private static final String MIME_TYPE = "audio/ogg";
+	private static final String PREVISUALIZACION_ARCHIVO = NOMBRE_ARCHIVO + "(" + TAMAÑO_ARCHIVO + " bytes, " + MIME_TYPE + ")";
+	
+	private Archivo audio;
+	
+	@Before
+	public void init() {
+		audio = new Audio(NOMBRE_ARCHIVO, CONTENIDO_ARCHIVO);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void obtenerNombreTest() {
+
+		assertEquals(NOMBRE_ARCHIVO, audio.obtenerNombre());
+	}
+	
+	@Test
+	public void obtenerContenidoTest() {
+
+		assertEquals(CONTENIDO_ARCHIVO, audio.obtenerContenido());
+	}
+	
+	@Test
+	public void obtenerTamañoTest() {
+
+		assertEquals(TAMAÑO_ARCHIVO, audio.obtenerTamaño());
+	}
+	
+	@Test
+	public void obtenerMimeTypeTest() {
+
+		assertEquals(MIME_TYPE, audio.obtenerMimeType());
+	}
+	
+	@Test
+	public void obtenerPreVisualizacionTest() {
+
+		assertEquals(PREVISUALIZACION_ARCHIVO, audio.obtenerPreVisualizacion());
 	}
 
 }
