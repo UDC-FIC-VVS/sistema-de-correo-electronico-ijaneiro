4e5f83135c56c91666da2ab0730e783ea89d8d9a
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-11 16:36:13 +0100

    Pruebas de Unidad: Imagen.

--- src/test/java/gal/udc/fic/vvs/email/archivo/ImagenTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/ImagenTest.java
@@ -2,13 +2,52 @@ package gal.udc.fic.vvs.email.archivo;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ImagenTest {
 
+	private static final String NOMBRE_ARCHIVO = "ImageTest";
+	private static final String CONTENIDO_ARCHIVO = "ImageTestContenido";
+	private static final int TAMAÑO_ARCHIVO = CONTENIDO_ARCHIVO.length();
+	private static final String MIME_TYPE = "image/png";
+	private static final String PREVISUALIZACION_ARCHIVO = NOMBRE_ARCHIVO + "(" + TAMAÑO_ARCHIVO + " bytes, " + MIME_TYPE + ")";
+	
+	private Archivo imagen;
+	
+	@Before
+	public void init() {
+		imagen = new Imagen(NOMBRE_ARCHIVO, CONTENIDO_ARCHIVO);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void obtenerNombreTest() {
+
+		assertEquals(NOMBRE_ARCHIVO, imagen.obtenerNombre());
+	}
+	
+	@Test
+	public void obtenerContenidoTest() {
+
+		assertEquals(CONTENIDO_ARCHIVO, imagen.obtenerContenido());
+	}
+	
+	@Test
+	public void obtenerTamañoTest() {
+
+		assertEquals(TAMAÑO_ARCHIVO, imagen.obtenerTamaño());
+	}
+	
+	@Test
+	public void obtenerMimeTypeTest() {
+
+		assertEquals(MIME_TYPE, imagen.obtenerMimeType());
+	}
+	
+	@Test
+	public void obtenerPreVisualizacionTest() {
+
+		assertEquals(PREVISUALIZACION_ARCHIVO, imagen.obtenerPreVisualizacion());
 	}
 
 }
