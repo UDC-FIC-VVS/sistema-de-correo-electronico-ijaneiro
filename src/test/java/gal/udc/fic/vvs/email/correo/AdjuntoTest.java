7ab208461d21d2cd198236a1f0f9939887f8663c
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-11 23:16:27 +0100

    Pruebas de Unidad: Adjunto.

--- src/test/java/gal/udc/fic/vvs/email/correo/AdjuntoTest.java
+++ src/test/java/gal/udc/fic/vvs/email/correo/AdjuntoTest.java
@@ -1,14 +1,40 @@
 package gal.udc.fic.vvs.email.correo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import gal.udc.fic.vvs.email.archivo.Archivo;
+import gal.udc.fic.vvs.email.archivo.Texto;
+
 public class AdjuntoTest {
+	
+	private Archivo archivo = new Texto("Texto","Texto");
+	private Mensaje mensaje = new Mensaje(new Texto("Correo1", "Correo de prueba"));
+	
+	private DecoradorMensaje adjunto;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void init() {
+		adjunto = new Adjunto(mensaje, archivo);
 	}
+	
+	@Test
+	public void obtenerTamañoTest() {
+		
+		int tamaño = mensaje.obtenerTamaño() + archivo.obtenerTamaño();
 
+		assertEquals(tamaño, adjunto.obtenerTamaño());
+	}
+	
+	@Test
+	public void obtenerVisualizacionTest() {
+		
+		String visualizacion = mensaje.obtenerVisualizacion() + "\n\nAdxunto: " + 
+				archivo.obtenerPreVisualizacion();
+		
+		assertEquals(visualizacion, adjunto.obtenerVisualizacion());
+	}
 }
