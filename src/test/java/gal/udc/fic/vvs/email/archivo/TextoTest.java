7f2e490549781c973b89b4154b63967d20203f28
Ivan Janeiro Tato <ivan.janeiro.tato@udc.es>
2021-01-11 16:36:53 +0100

    Pruebas de Unidad: Texto. Acabadas pruebas de Unidad del paquete: Archivo

--- src/test/java/gal/udc/fic/vvs/email/archivo/TextoTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/TextoTest.java
@@ -2,13 +2,51 @@ package gal.udc.fic.vvs.email.archivo;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextoTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private static final String NOMBRE_ARCHIVO = "TextoTest";
+	private static final String CONTENIDO_ARCHIVO = "TextoTestContenido";
+	private static final int TAMAÑO_ARCHIVO = CONTENIDO_ARCHIVO.length();
+	private static final String MIME_TYPE = "text/plain";
+	private static final String PREVISUALIZACION_ARCHIVO = NOMBRE_ARCHIVO + "(" + TAMAÑO_ARCHIVO + " bytes, " + MIME_TYPE + ")";
+	
+	private Archivo texto;
+	
+	@Before
+	public void init() {
+		texto = new Texto(NOMBRE_ARCHIVO, CONTENIDO_ARCHIVO);
 	}
 
+	@Test
+	public void obtenerNombreTest() {
+
+		assertEquals(NOMBRE_ARCHIVO, texto.obtenerNombre());
+	}
+	
+	@Test
+	public void obtenerContenidoTest() {
+
+		assertEquals(CONTENIDO_ARCHIVO, texto.obtenerContenido());
+	}
+	
+	@Test
+	public void obtenerTamañoTest() {
+
+		assertEquals(TAMAÑO_ARCHIVO, texto.obtenerTamaño());
+	}
+	
+	@Test
+	public void obtenerMimeTypeTest() {
+
+		assertEquals(MIME_TYPE, texto.obtenerMimeType());
+	}
+	
+	@Test
+	public void obtenerPreVisualizacionTest() {
+
+		assertEquals(PREVISUALIZACION_ARCHIVO, texto.obtenerPreVisualizacion());
+	}
 }
